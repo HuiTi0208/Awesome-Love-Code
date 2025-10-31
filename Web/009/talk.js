f2eb7d3b98e90923acec43a62e3b9cd2bc351f69
HuiTi0208 <songrou758@gmail.com>
2025-10-31 22:27:59 +0800

    Update talk.js

--- Web/009/talk.js
+++ Web/009/talk.js
@@ -14,56 +14,30 @@ $(function() {
 });
 
 function A() {
-    modal('我爱你！', B);
+    modal('我很開心', B);
 }
 
 function B() {
-    modal('我知道你在等我这一句话', C);
+    modal('我希望之後的每天', C);
 }
 
 function C() {
-    modal('请宝宝不要拒绝我嘛~', D);
+    modal('都能夠有妳', D);
 }
 
 function D() {
-    modal('拒绝我，不存在的', E);
+    modal('我對妳的愛', E);
 }
 
 function E() {
-    modal('这辈子都不可能让你离开我的!!!', F);
-}
-
-function F() {
-    modal('跟我走吧宝宝~', G);
-}
-
-function G() {
-    modal('房产证上写你的名字', H);
-}
-
-function H() {
-    modal('我会做饭', I);
-}
- function I() {
-    modal('我会买很多东西给你', J)
-}
-function I() {
-    modal('会好好疼你', J)
-}
-function I() {
-    modal('会哄你睡觉', J)
-}
-function I() {
-    modal('会唱歌给你听', J)
-}
-function I() {
-    modal('爱你。么么哒！', J)
-}
-
-function J() {
-    modal('行，我们去民政局登记吧', function() {
+    modal('永遠都不會改變!', function() {
         fireworks();
     });
+}
+
+function fireworks() {
+    $('.page_one').addClass('hide');
+    initAnimate();
 }
 
 function fireworks() {
